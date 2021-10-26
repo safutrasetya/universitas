f73ec0a6244211e2d83617c04d9b40cfc30603da
Marco M Mahulae <91104335+MarcoMahulae@users.noreply.github.com>
2021-10-26 17:13:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- css buat -->
+    <link rel="stylesheet" href="index.css">
+
+    <title>Index</title>
+  </head> 
+  <body>
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Explore</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"   aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse text-center" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                 <a class="nav-link" href="#About">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Contactus">ContactUs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Univesityfinder">UnivesityFinder</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Detail">Detail</a>
+              </li>
+            </ul>
+         </div>
+       </div>
+    </nav>
+    <!--navbar-->
+    <!-- jumbotron-->
+  <div class="container-fluid banner">
+      <div class="container text-center">
+        <h4 class="display-6">Selamat Datang di Website Kami</h4>
+        <h3 class="display-1">hai!hallo!</h3>
+      </div>
+  </div>
+
+  <!--javascript bootsrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   <!--javascript  bootsrap-->
+  </body>
+</html>
