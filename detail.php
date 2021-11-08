501f91ac58e537262adebd7b034c312d5ea7a128
fadil0711 <outher.heaven07@gmail.com>
2021-11-08 15:41:03 +0700

    hal detail

---
+++ detail.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- css buat -->
+    <link rel="stylesheet" href="index.css">
+
+    <title>Index</title>
+  </head>
+  <body>
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Explore</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"   aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+          </div>
+        </div>
+      </div>
+    </nav>
+  <!--end navbar-->
+
+  <section id="about">   <div class="container">
+       <div class="row">
+         <div class="col text-center mb-3">
+
+             <h1>Universitas Sumatera Utara</h1>
+             <p>Kode Universitas : USU</p>
+         </div>
+       </div>
+       <div class="jumbotron">
+        <div class="card-body">
+          <p>
+            Universitas Sumatera Utara (USU) is one of the prestigious academic institution in Indonesia located in Medan, North Sumatra. USU is the oldest and one of the biggest public universities outside Java that was initiated by the establishment of University of Sumatera Utara Foundation on June 4, 1952, pioneered by Abdul Hakim, the Governor of North Sumatra.
+
+            USU has two campuses, Padang Bulan Campus and Kwala Bekala Campus. The main Padang Bulan Campus occupies a green area of approximately 120 hectares. Padang Bulan Campus accommodates all lecturing, practicum and research activities for more than 50,000 students. Academic activities in Kwala Bekala Campus are focused on agroforestry, animal husbandry, poultry and environmental management sustainability.
+
+            USU emphasizes on TALENTA as the competitive advantages for research and academic development. TALENTA stands for Tropical Science & Medicine, Agro-industry, Local Wisdom, Energy (Sustainable), Natural Resources, Technology Appropriate, and Arts.
+          </p>
+        </div>
+      </div>
+
+<img src="https://2.bp.blogspot.com/-EQNsLJiDxwg/VsLDDzUxdNI/AAAAAAAAERo/kO_RI7laz0g/s1600/logo%2Busu%2Buntuk%2Bsemua.png" width="250px" height="250px" alt="" class="img-thumbnail">
+
+    <!--javascript bootsrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+   <!--javascript  bootsrap-->
+  </body>
+</html>
