f5b16008c7d098300282aab5c1084961aad7f59d
Marco M Mahulae <91104335+MarcoMahulae@users.noreply.github.com>
2021-11-12 12:25:52 +0700

    Create index.php

--- index.php
+++ index.php
@@ -24,7 +24,9 @@
             <div class="navbar-nav ms-auto">
               <a class="nav-link active" aria-current="page" href="#">Home</a>
               <a class="nav-link" href="about.php">About Us</a>
+              <a class="nav-link" href="universitasfinder.php">Finder</a>
               <a class="nav-link" href="contact.php">Contact Us</a>
+               
     
             </div>
           </div>
